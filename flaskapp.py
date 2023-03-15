539414264ed75a3c37b5cf151d116b4471cd5c50
kayky <1787963736@qq.com>
2023-03-15 02:24:39 +0000

    chatgpt搭建完毕，功能正常

---
+++ flaskapp.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template, redirect,jsonify
+import openai
+import os
+from dotenv import load_dotenv
+from revChatGPT.V3 import Chatbot
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+app = Flask(__name__)
+
+def send_gpt(prompt):
+    try:
+        response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        #model = 'text-davinci-003',
+        messages=[{"role": "user", "content": prompt}]
+        )
+        return response["choices"][0]['message']['content']
+    except Exception as e:
+        return e
+@app.route("/")
+def index():
+    return render_template("chat.html")
+
+@app.route("/get", methods=['GET'])
+def get_request_json():
+    
+    question = request.args.get('msg')
+    print("======================================")
+    print("Receive the question:", question)
+    res = send_gpt(question)
+
+    print("Q：\n", question)
+    print("A：\n", res)
+    return res
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
